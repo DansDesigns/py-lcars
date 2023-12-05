d3c7f96d00d433b3af3f8b2a6722110fd1cbeacc
DanDan <72343266+DansDesigns@users.noreply.github.com>
2023-12-05 20:05:25 +0000

    Update authorize.py

--- app/screens/authorize.py
+++ app/screens/authorize.py
@@ -17,7 +17,7 @@ class ScreenAuthorize(LcarsScreen):
                         layer=0)
 
         #all_sprites.add(LcarsGifImage("assets/gadgets/stlogorotating.gif", (103, 369), 50), 
-                        layer=0)        
+        #                layer=0)        
 
         all_sprites.add(LcarsText(colours.ORANGE, (270, -1), "AUTHORIZATION REQUIRED", 2),
                         layer=0)
