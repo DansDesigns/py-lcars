081a8d7fd386abfe42f452b0edc9d6caaa2a8594
DanDan <72343266+DansDesigns@users.noreply.github.com>
2023-12-05 19:40:33 +0000

    Update lcars.py

--- app/lcars.py
+++ app/lcars.py
@@ -5,7 +5,7 @@ import config
 if __name__ == "__main__":
     firstScreen = ScreenAuthorize()
     ui = UserInterface(firstScreen, config.RESOLUTION, config.UI_PLACEMENT_MODE, config.FPS, config.DEV_MODE,
-                       config.SOUND, config.CAL)
+                       config.SOUND)
 
     while (True):
         ui.tick()
